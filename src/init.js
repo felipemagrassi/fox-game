f101f54d21c1cb9584c94eca86aa659640151caa
Felipe Magrassi <felipe.1magrassi@gmail.com>
2021-07-01 14:37:09 -0300

    finished

---
+++ src/init.js
@@ -0,0 +1,26 @@
+import game, { handleUserAction } from "./gamestate";
+import initButtons from "./buttons";
+import { TICK_RATE } from "./constants";
+/**
+ * Tick-rate Method for javascript
+ * init() starts the requestAnimationFrame that runs once with requestAnimationFrame
+ * then we create a endless closure loop calling requestAnimationFrame inside the closure function
+ */
+async function init() {
+  console.log("starting game");
+  initButtons(handleUserAction);
+
+  let nextTimeToTick = Date.now();
+  function nextAnimationFrame() {
+    const now = Date.now();
+    if (nextTimeToTick <= now) {
+      game.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  nextAnimationFrame();
+}
+
+init();
